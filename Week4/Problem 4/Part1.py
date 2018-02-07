bb5393936893c2367adbad37e77f18d70ad27a49
sanjeevr11 <sanjeevkumarrai96@gmail.com>
2018-02-08 00:28:08 +0530

    Part 1

---
+++ Week4/Problem 4/Part1.py
@@ -0,0 +1,8 @@
+def getWordScore(word, n):
+    score = 0
+    for i in word:
+        score += SCRABBLE_LETTER_VALUES[i]
+    if len(word) == n:
+        return (score * len(word)) + 50
+    else:
+        return score * len(word)
